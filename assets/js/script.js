28ccbec983bd9d021b65466be0483e05fbcd369f
conor-b1995 <conor_carricktown@hotmail.com>
2022-06-12 16:37:06 +0000

    Add code to check the answer and rspond to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,17 +4,17 @@ document.addEventListener('DOMContentLoaded', function(){
     for (let button of buttons) {
         button.addEventListener('click', function(){
             if (this.getAttribute('data-type') === 'submit') {
-                alert('You clicked Submit!');
+                checkAnswer();
             }
             else {
                 let gameType = this.getAttribute('data-type');
                 runGame(gameType);
             }
-        })
+        });
     }
 
     runGame('addition'); 
-})   
+});   
 
 /**
  * The main game "loop", called when the script is first loaded 
@@ -35,11 +35,45 @@ function runGame(gameType) {
     }
 }
 
-function checkAnswer () {
+/**
+ * Checks the answer against the first element in 
+ * the returned calculateCorrectAnswer array
+ */
 
+function checkAnswer () {
+    
+    let userAnswer = parseInt(document.getElementById('answer-box').value);
+    let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculatedAnswer[0];
+
+    if (isCorrect) {
+        alert('Hey! You got it right! :D');
+    }
+    else {
+        alert(`Aww...you answered ${userAnswer}. The correct answer is ${calculatedAnswer[0]}!`);
+    }
+
+    runGame(calculatedAnswer[1]);
 }
 
+/**
+ * Gets the operands (the numbers) and the operator (+, - etc)
+ * directly from the dom and returns the correct answer.
+ */
+
 function calculateCorrectAnswer () {
+
+    let operand1 = parseInt(document.getElementById('operand1').innerText);
+    let operand2 = parseInt(document.getElementById('operand2').innerText);
+    let operator = document.getElementById('operator').innerText;
+
+    if (operator === '+') {
+        return [operand1 + operand2, 'addition'];
+    }
+    else {
+        alert(`Unimplemented operator ${operator}`);
+        throw `Unimplemented operator ${operator}. Aborting!`;
+    }
 
 }
 
@@ -52,11 +86,11 @@ function imcrementWrongAnswer () {
 }
 
 function displayAdditionQuestion (operand1, operand2) {
+
      document.getElementById('operand1').textContent = operand1;
      document.getElementById('operand2').textContent = operand2;
      document.getElementById('operator').textContent = '+';
-
-     
+      
 }
 
 function displaySubtractQuestion () {
