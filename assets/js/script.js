10f5798b318bda38f4c1f554aa936edfe1765aa0
conor-b1995 <conor_carricktown@hotmail.com>
2022-06-12 15:04:56 +0000

    Display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,16 +8,31 @@ document.addEventListener('DOMContentLoaded', function(){
             }
             else {
                 let gameType = this.getAttribute('data-type');
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         })
     }
+
+    runGame('addition'); 
 })   
 
+/**
+ * The main game "loop", called when the script is first loaded 
+ * and after the users answer has been processed 
+ */
+function runGame(gameType) {
 
-function runGame() {
+    // Creates to random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === 'addition') {
+        displayAdditionQuestion(num1, num2);
+    }
+    else {
+        alert(`Unknown game type ${gameType}`);
+        throw `Unkown game type ${gameType}. Aborting!`;
+    }
 }
 
 function checkAnswer () {
@@ -36,8 +51,12 @@ function imcrementWrongAnswer () {
 
 }
 
-function displayAdditionQuestion () {
-    
+function displayAdditionQuestion (operand1, operand2) {
+     document.getElementById('operand1').textContent = operand1;
+     document.getElementById('operand2').textContent = operand2;
+     document.getElementById('operator').textContent = '+';
+
+     
 }
 
 function displaySubtractQuestion () {
