d7bd4581ae5a932876b966e4b2c6a56d0ded2fc6
conor-b1995 <conor_carricktown@hotmail.com>
2022-06-13 13:28:15 +0000

    Add structure to division section

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,6 +44,9 @@ function runGame(gameType) {
     else if (gameType === 'subtract') {
         displaySubtractQuestion(num1, num2)
     }
+    else if (gameType === 'division') {
+        displayDivisionQuestion(num1, num2)
+    }
     else {
         alert(`Unknown game type ${gameType}`);
         throw `Unkown game type ${gameType}. Aborting!`;
@@ -92,6 +95,9 @@ function calculateCorrectAnswer () {
     }
     else if (operator === '-') {
         return [operand1 - operand2, 'subtract']
+    }
+    else if (operator === '/') {
+        return [operand1 / operand2, 'division']
     }
     else {
         alert(`Unimplemented operator ${operator}`);
@@ -143,4 +149,13 @@ function displayMultiplyQuestion (operand1, operand2) {
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = 'x';
      
+}
+
+function displayDivisionQuestion (operand1, operand2) {
+    
+    document.getElementById('operand1').textContent = operand1 * operand2;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = '/';
+
+
 }
